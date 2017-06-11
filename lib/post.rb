485d1b2334d4b532a962a7b2d811710ec4db57ec
yfern328 <yfernan2@u.rochester.edu>
2017-06-11 18:37:54 -0400

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,13 @@
+class Post
+
+  attr_accessor :title, :author
+
+  def initialize(title)
+    @title = title
+  end
+
+  def author_name
+    self.author == nil ? nil : self.author.name
+  end
+
+end
---
+++ lib/song.rb
@@ -0,0 +1,13 @@
+class Song
+
+  attr_accessor :name, :artist
+
+  def initialize(name)
+    @name = name
+  end
+
+  def artist_name
+    self.artist == nil ? nil : self.artist.name
+  end
+
+end
